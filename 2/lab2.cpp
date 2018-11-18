781023b1cd86de27978cd9cab7b3059431c7e674
Vadim <cerber9283@gmail.com>
2018-11-18 17:55:48 +0300

    lab2 is ready

--- 2/lab2.cpp
+++ 2/lab2.cpp
@@ -11,16 +11,11 @@ using namespace std;
 void lab2()
 {
     Mat img = imread("../4/ig_0.jpg", CV_LOAD_IMAGE_COLOR);
-    //cv::Mat img = cv::imread("/home/bledgharm/CV_labs/labs/2/images1.jpg", CV_LOAD_IMAGE_COLOR);
     Mat lib_blur = img.clone();
     Mat cust_blur = img.clone();
-
     Mat compare = img.clone();
 
     Size filter_size(3, 3);
-    // Start processing from first pixel (0,0)
-//    Point startROI(-1, -1);
-//    Rect ROI = Rect(startROI, filter_size);
 
     TickMeter msec_timer;
 
@@ -47,63 +42,47 @@ void lab2()
 //    imshow("Original", img);
 
     waitKey(0);
-    //return 0;
 }
 
 /// May use locateROY()
 /// use Mat.ptr to get start of row
 void custom_blur(const Mat orig, Mat res, Size filter_size)
 {
-    int width = res.cols - 2;
-    int height = res.rows - 2;
-    int imgSize = (res.cols - 2) * (res.rows - 2);
-
     // Need to expand original image and make non-filtering frame
     // After processing delete this frame
+    int width = res.cols - 2;
+    int imgSize = (res.cols - 2) * (res.rows - 2);
+
     Point startROI(0, 0);
     Rect ROI = Rect(startROI, filter_size);
     int sizeROI = filter_size.height * filter_size.width;
 
-//    for (int i = 0; i < imgSize; i++)
-    for (int y = 0; y < height; y++)
+    for (int i = 0; i < imgSize; i++)
     {
-        ROI.y = y;
-        for (int x = 0; x < width; x++)
+        ROI.x = i % width;
+        ROI.y = i / width;
+        Mat image_ROI = res(ROI);
+
+        int blue = 0;
+        int green = 0;
+        int red = 0;
+
+        int xInROI = 0;
+        int yInROI = 0;
+        for (int j = 0; j < sizeROI; j++)
         {
-//            ROI.x = i % width;
-//            ROI.y = i / width;
-            ROI.x = x;
-            Mat image_ROI = res(ROI);
-            // Change pixels inside img_ROI
-//        int x = 0;
-//        int y = 0;
-
-            int blue = 0;
-            int green = 0;
-            int red = 0;
-
-            for (int i = 0; i < 3; i++)
-            {
-                for (int j = 0; j < 3; j++)
-                {
-//                    x = j % filter_size.width;
-//                    y = j / filter_size.height;
-                    blue += image_ROI.at<Vec3b>(i, j)[0];
-                    green += image_ROI.at<Vec3b>(i, j)[1];
-                    red += image_ROI.at<Vec3b>(i, j)[2];
-                }
-            }
-            blue /= 9;
-            green /= 9;
-            red /= 9;
-//            x = ROI.x + 1;
-//            y = ROI.y + 1;
-//        cout << endl << "old blue is: " << static_cast<int>(image_ROI.at<Vec3b>(x,y)[0] ) << endl;
-            image_ROI.at<Vec3b>(1, 1)[0] = blue;
-//        cout << "new blue is: " << static_cast<int>(image_ROI.at<Vec3b>(x,y)[0] ) << endl;
-            image_ROI.at<Vec3b>(1, 1)[1] = green;
-            image_ROI.at<Vec3b>(1, 1)[2] = red;
+            xInROI = j % filter_size.width;
+            yInROI = j / filter_size.height;
+            blue += image_ROI.at<Vec3b>(yInROI, xInROI)[0];
+            green += image_ROI.at<Vec3b>(yInROI, xInROI)[1];
+            red += image_ROI.at<Vec3b>(yInROI, xInROI)[2];
         }
+        blue /= 9;
+        green /= 9;
+        red /= 9;
+        image_ROI.at<Vec3b>(1, 1)[0] = blue;
+        image_ROI.at<Vec3b>(1, 1)[1] = green;
+        image_ROI.at<Vec3b>(1, 1)[2] = red;
     }
 }
 
@@ -112,49 +91,26 @@ float compare_blurs(Mat compare1, Mat compare2)
 {
     int width = compare1.cols;
     int height = compare1.rows;
-    int blue1 = 0, green1 = 0, red1 = 0;
-    int blue2 = 0, green2 = 0, red2 = 0;
-    int simil;
+    float blue1 = 0, green1 = 0, red1 = 0;
+    float blue2 = 0, green2 = 0, red2 = 0;
+    float simil;
 
     for (int i=0; i<width; i++)
     {
         for (int j = 0; j < height; j++)
         {
             // Gaining intensity of first image
-            Vec3b elem = compare1.at<Vec3b>(i, j);
-            blue1  += elem.val[0]/10;
-            green1 += elem.val[1]/10;
-            red1   += elem.val[2]/10;
+            blue1  += compare1.at<Vec3b>(i, j)[0]/10.0;
+            green1 += compare1.at<Vec3b>(i, j)[1]/10.0;
+            red1   += compare1.at<Vec3b>(i, j)[2]/10.0;
 
             // Getting ratio of intensity with second image
-            elem = compare2.at<Vec3b>(i, j);
-            blue2  += elem.val[0]/10;
-            green2 += elem.val[1]/10;
-            red2   += elem.val[2]/10;
+            blue2  += compare2.at<Vec3b>(i, j)[0]/10.0;
+            green2 += compare2.at<Vec3b>(i, j)[1]/10.0;
+            red2   += compare2.at<Vec3b>(i, j)[2]/10.0;
         }
     }
     simil = (blue1+green1+red1)/((blue2+green2+red2));
+    simil = (simil < 1) ? simil : (2 - simil);
     return simil;
 }
-
-//void my3x3FilterBox(Mat orig, Mat res, int x0, int y0)
-//{
-//	Rect rp(y0-1, x0-1, 3, 3);
-//	Mat roi = orig(rp);
-//	int blue = 0, green = 0, red = 0;
-//
-//	for (int i=0; i<3; i++)
-//		for (int j = 0; j < 3; j++)
-//		{
-//			Vec3b elem = roi.at<Vec3b>(i, j);
-//			blue += elem.val[0];
-//			green += elem.val[1];
-//			red += elem.val[2];
-//		}
-//
-//	blue /= 9;
-//	green /= 9;
-//	red /= 9;
-//	Vec3b newElem = Vec3b(blue, green, red);
-//	res.at<Vec3b>(x0, y0) = newElem;
-//}
